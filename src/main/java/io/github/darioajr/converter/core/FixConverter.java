dc20606209f0136f04c91d8fad083e5a4cd4f6b3
DARIO ALVES JUNIOR <dario.junior@ciahering.com.br>
2025-01-15 14:40:00 -0300

    fix(tests): add new tests

--- src/main/java/io/github/darioajr/converter/core/FixConverter.java
+++ src/main/java/io/github/darioajr/converter/core/FixConverter.java
@@ -1,6 +1,7 @@
 package io.github.darioajr.converter.core;
 
 import io.github.darioajr.converter.utils.AvroUtils;
+import java.io.IOException;
 import org.apache.avro.generic.GenericRecord;
 
 /**
@@ -15,5 +16,14 @@ public class FixConverter implements Converter<String, GenericRecord> {
   @Override
   public GenericRecord convertToAvro(String rawMessage, SchemaProvider schema) {
     return AvroUtils.convertFixToAvro(rawMessage, schema);
+  }
+
+  /**
+   * Implementation of convertToByteArray.
+   *
+   * @throws IOException if an I/O error occurs during conversion
+   */
+  public byte[] convertToByteArray(String rawMessage, SchemaProvider schema) throws IOException {
+    return AvroUtils.convertFixToAvroByteArray(rawMessage, schema);
   }
 }
